9e7b5a5188da1b442ddbf15da46f8fcb601273b3
jobeda123 <jobedanur265@gmail.com>
2025-08-07 13:24:07 +0600

    fix download button issue

--- src/components/core/button/DownloadButton.tsx
+++ src/components/core/button/DownloadButton.tsx
@@ -1,10 +1,12 @@
-import { CONTACT_INFO } from '@/constants/user'
 import React from 'react'
+import { CONTACT_INFO } from '@/constants/user'
+
 
 function DownloadButton() {
     return (
         <a
             href={CONTACT_INFO.cv}
+            target='_blank'
             download
             className="w-full mt-4 px-4 py-2 bg-white/10 hover:bg-white/20 text-white border border-white/20 rounded-lg transition-all duration-300 hover:border-white/40 flex items-center justify-center space-x-2 cursor-pointer text-center"
         >
