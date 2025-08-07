5ead2c29f93718ae8667282242fcc5af19d36394
jobeda123 <jobedanur265@gmail.com>
2025-08-07 12:46:27 +0600

    portfolio icon change

--- src/components/icons/AllIcons.tsx
+++ src/components/icons/AllIcons.tsx
@@ -41,7 +41,7 @@ export const PortfolioIcon = ({ className = "w-6 h-6" }: { className?: string })
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
-      d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
     />
   </svg>
 );
